5ca48fd4d2af2cd1d9a64a3a06ff8e5b518a81ae
Ru <rudmat@rambler.com>
2016-02-09 20:04:11 +0100

    5.16 5.17 5.18 settings_action_bar + redlines_list_items + redlines_finish

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends ActionBarActivity  implements ForecastFragment
             }
         } else {
             mTwoPane = false;
-            getSupportActionBar().setElevation(0f);
+//            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
